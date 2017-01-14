99c17305ff932a3c26a0024e5f604df159d21c3b
mdtmc <miles.cranmer@gmail.com>
2017-01-14 10:10:00 -0500

    Some dummy proofing

--- timer.py
+++ timer.py
@@ -6,18 +6,8 @@ from Tkinter import *
 def timer(minutes):
 	print "Started at " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 	print "Running for %f minutes" %(minutes)
-	mins = 0
-	#60 seconds of sleep
-	sec = 60
-	#can also call less than a minute
-	if minutes < 1:
-		sec = int(60*minutes)
-    #Loop until minutes is reached
-	while mins < minutes:
-		time.sleep(sec)
-		#One more minute!
-		mins += 1
-    #when time is over, create dialog box:
+	time.sleep(int(60.0*minutes))
+        #when time is over, create dialog box:
 	root = Tk() 
 	root.title("%d minutes are up!" % (minutes))
 	root.lift()
@@ -26,7 +16,8 @@ def timer(minutes):
 
 #starts program with entered time in minutes, or 10 minutes
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		timer(float(sys.argv[1]))
-	else:
-		timer(10)
+    if len(sys.argv) > 1:
+        timer(float(sys.argv[1]))
+    else:
+        print("Please enter a time")
+        exit()
